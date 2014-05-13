eac07d0e0e321177049bb9cfd69736612f9a5b7b
Christine Wilks <crissxross@gmail.com>
2014-05-13 23:17:35 +0100

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('cxshowcaseApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
