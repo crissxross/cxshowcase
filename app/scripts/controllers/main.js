63696abbe9d3593b9719ef71603fddbded86916f
Christine Wilks <crissxross@gmail.com>
2014-05-17 03:45:09 +0100

    Implemented showcase ng-repeat (kept hard-coded showcase items for comparison)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('cxshowcaseApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('showcase.json').success(function(data) {
+      $scope.showcase = data;
+    }).error(function(data) {
+      // log error
+    });
   });
