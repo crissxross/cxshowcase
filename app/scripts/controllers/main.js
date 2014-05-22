0180836bd5a34bb3ec5b1d3540a23ddaad7e410a
Christine Wilks <crissxross@gmail.com>
2014-05-22 14:27:39 +0100

    Added other works to main controller & html in a well

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,4 +7,60 @@ angular.module('cxshowcaseApp')
     }).error(function(data) {
       // log error
     });
+    $scope.otherworks = [
+      {
+        title: 'I/O Out of Touch',
+        workUrl: 'http://crissxross.net/oot/indexoot.html',
+        description: 'for live digital text performance',
+        imageUrl: '',
+        pubdate: '2012',
+        tech: 'Flash',
+        moreinfo: ''
+      },
+      {
+        title: 'Heights',
+        workUrl: 'http://crissxross.net/MovingPix/Heights.html',
+        description: 'animated e-poem',
+        imageUrl: '',
+        pubdate: '2006',
+        tech: 'Flash',
+        moreinfo: ''
+      },
+      {
+        title: 'IntraVenus',
+        workUrl: 'http://crissxross.net/MovingPix/IntraVenus.html',
+        description: 'musing on the artist\'s muse',
+        imageUrl: '',
+        pubdate: '2005',
+        tech: 'Flash',
+        moreinfo: ''
+      },
+      {
+        title: 'We Drank',
+        workUrl: 'http://crissxross.net/MovingPix/We_Drank.html',
+        description: 'animated e-poem',
+        imageUrl: '',
+        pubdate: '2005',
+        tech: 'Flash',
+        moreinfo: ''
+      },
+      {
+        title: 'Social Dis-Ease',
+        workUrl: 'http://crissxross.net/MovingPix/SocialDis-Ease.html',
+        description: 'animated e-poem',
+        imageUrl: '',
+        pubdate: '2004',
+        tech: 'Flash',
+        moreinfo: ''
+      },
+      {
+        title: 'Sitting Pretty',
+        workUrl: 'http://crissxross.net/MovingPix/BeforeWeBegin/index.html',
+        description: 'hypertext with multimedia',
+        imageUrl: '',
+        pubdate: '2004',
+        tech: 'HTML',
+        moreinfo: ''
+      }
+    ];
   });
