ee25a29001a602b1d0c40a27c8aaa048b9ca4fdd
Christine Wilks <crissxross@gmail.com>
2014-05-23 14:51:38 +0100

    Fixed error in main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,8 +4,6 @@ angular.module('cxshowcaseApp')
   .controller('MainCtrl', function ($scope, $http) {
     $http.get('showcase.json').success(function(data) {
       $scope.showcase = data;
-    }).error(function(data) {
-      // log error
     });
     $scope.otherworks = [
       {
